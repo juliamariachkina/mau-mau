c7e131608fa8562efa2ae22b594ce06c9e22f0d6
julia mar <junami.mar@gmail.com>
2022-08-19 12:20:10 +0200

    Add suit choice modal & small refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,10 @@ import { Card as CardType, deck, Suit, Rank, SUITS } from "./types/card";
 import { Player as PlayerType, Position } from "./types/player";
 import { getImageSrc } from "./Utility";
 import { v4 as uuidv4 } from 'uuid';
+import { Modal, SuitChoiceModal } from "./Modal";
 
 const PLAYER_TURN_TIMEOUT = 1200;
-const INIT_CARDS_COUNT_PER_PLAYER = 1;
+const INIT_CARDS_COUNT_PER_PLAYER = 4;
 
 export function App(props: {}) {
     const [stock, setStock] = useState<CardType[]>([]);
@@ -22,6 +23,8 @@ export function App(props: {}) {
     const [activeSuit, setActiveSuit] = useState(SUITS[0]);
     const [nextStockDrawCount, setNextStockDrawCount] = useState(1);
     const [beginRound, setBeginRound] = useState(true);
+    const [renderFinishModal, setRenderFinishModal] = useState(false);
+    const [renderSuitChoiceModal, setRenderSuitChoiceModal] = useState(false);
 
     useEffect(() => {
         if (!beginRound) {
@@ -52,43 +55,48 @@ export function App(props: {}) {
         setActiveSuit(discardPileTop.suit);
         getNextStockDrawCount(discardPileTop.rank === "A" || discardPileTop.rank === "7", discardPileTop, 1);
         setBeginRound(false);
+        setRenderFinishModal(false);
+        setRenderSuitChoiceModal(false);
     });
 
-    function getCurrentPlayer(): PlayerType {
-        const player = players.find(p => p.id === currentPlayerId);
-        if (player === undefined) {
-            if (!beginRound) {
-                throw Error("Player with current player id isn't found");
-            }
-            return { id: "", isReal: false, cards: [], position: "left" };
-        }
-        return player;
-    }
-
-    function getNextPlayerId() {
-        const index = players.findIndex(p => p.id === currentPlayerId);
-        return players[(index + 1) % players.length].id;
-    }
-
     useEffect(() => {
-        if (currentPlayerId !== "" && !getCurrentPlayer().isReal) {
+        if (currentPlayerId !== ""
+            && !getCurrentPlayer(players, currentPlayerId, beginRound).isReal
+            && !renderFinishModal
+            && !renderSuitChoiceModal) {
             setTimeout(makeTurnArtificialPlayer, PLAYER_TURN_TIMEOUT);
         }
-    }, [currentPlayerId]);
+    }, [currentPlayerId, renderFinishModal, renderSuitChoiceModal]);
 
     const handleCardClick: MouseEventHandler<HTMLImageElement> = (e) => {
         makeTurnRealPlayer(e.target);
     }
 
+    const handleModalClose = () => {
+        setBeginRound(true);
+    }
+
+    const handleSuitChoice = (target: HTMLImageElement) => {
+        if (target.src.includes("club")) {
+            setActiveSuit("club");
+        }
+        if (target.src.includes("diamond")) {
+            setActiveSuit("diamond");
+        }
+        if (target.src.includes("spade")) {
+            setActiveSuit("spade");
+        }
+        if (target.src.includes("heart")) {
+            setActiveSuit("heart");
+        }
+        setRenderSuitChoiceModal(false);
+    }
+
     const handleSkipTurnClick = () => {
-        if (!getCurrentPlayer().isReal) {
+        if (!getCurrentPlayer(players, currentPlayerId, beginRound).isReal) {
             return;
         }
-        if (nextStockDrawCount === 0) {
-            setCurrentPlayerId(getNextPlayerId());
-            setCardEffect(false);
-            setNextStockDrawCount(1);
-        } else {
+        if (nextStockDrawCount !== 0) {
             const [stockAfterDrawing, discardPileAfterDrawing, drawnCards] = drawCards(stock, discardPile, nextStockDrawCount);
             setStock(stockAfterDrawing);
             setDiscardPile(discardPileAfterDrawing);
@@ -98,10 +106,10 @@ export function App(props: {}) {
                 }
                 return p;
             }));
-            setCurrentPlayerId(getNextPlayerId());
-            setCardEffect(false);
-            setNextStockDrawCount(1);
         }
+        setCurrentPlayerId(getNextPlayerId(players, currentPlayerId));
+        setCardEffect(false);
+        setNextStockDrawCount(1);
     }
 
     function drawFromStockRealPlayer() {
@@ -114,7 +122,7 @@ export function App(props: {}) {
             }
             return p;
         }));
-        setCurrentPlayerId(getNextPlayerId());
+        setCurrentPlayerId(getNextPlayerId(players, currentPlayerId));
         setCardEffect(false);
         setNextStockDrawCount(1);
     }
@@ -123,34 +131,33 @@ export function App(props: {}) {
         if (!canBePlayedOn(clickedOnCard, discardPile, activeSuit, isCardEffectOn)) {
             return false;
         }
-        if (getCurrentPlayer().cards.length === 1) {
-            //Game over: success
-            setBeginRound(true);
-            return true;
-        }
         setDiscardPile([...discardPile, clickedOnCard]);
-        setPlayers(players.map(p => {
-            if (p.id === currentPlayerId) {
-                return { ...p, cards: p.cards.filter(card => !getImageSrc(clickedOnCard).includes(getImageSrc(card))) };
-            }
-            return p;
-        }));
-        setCurrentPlayerId(getNextPlayerId());
+        setPlayers(players
+            .map(p => {
+                if (p.id === currentPlayerId) {
+                    return { ...p, cards: p.cards.filter(card => !getImageSrc(clickedOnCard).includes(getImageSrc(card))) };
+                }
+                return p;
+            })
+            .filter(p => p.cards.length !== 0));
+        setCurrentPlayerId(getNextPlayerId(players, currentPlayerId));
         setCardEffect(clickedOnCard.rank === "7" || clickedOnCard.rank === "A");
         setActiveSuit(clickedOnCard.suit);
         setNextStockDrawCount(getNextStockDrawCount(clickedOnCard.rank === "7" || clickedOnCard.rank === "A", clickedOnCard, nextStockDrawCount));
+        setRenderFinishModal(getCurrentPlayer(players, currentPlayerId, beginRound).cards.length === 1);
+        setRenderSuitChoiceModal(clickedOnCard.rank === "Q");
         return true;
     }
 
     function makeTurnRealPlayer(target: EventTarget) {
-        if (!getCurrentPlayer().isReal) {
+        if (!getCurrentPlayer(players, currentPlayerId, beginRound).isReal) {
             return;
         }
         if (target === document.querySelector(".stock")) {
             drawFromStockRealPlayer();
             return;
         }
-        const clickedOnCard = getCurrentPlayer().cards
+        const clickedOnCard = getCurrentPlayer(players, currentPlayerId, beginRound).cards
             .find(card => (target as HTMLImageElement).getAttribute("src")?.includes(getImageSrc(card)));
         if (clickedOnCard === undefined) {
             throw Error("User didn't click on stock or any of his cards, but the card click event handler " +
@@ -160,60 +167,63 @@ export function App(props: {}) {
     }
 
     function makeTurnArtificialPlayer() {
-        if (!tryPlayCardArtificialPlayer()) {
-            const [stockAfterDrawing, discardPileAfterDrawing, drawnCards] = drawCards(stock, discardPile, nextStockDrawCount);
-            setStock(stockAfterDrawing);
-            setDiscardPile(discardPileAfterDrawing);
-            setPlayers(players.map(p => {
-                if (p.id === currentPlayerId) {
-                    return { ...p, cards: [...p.cards, ...drawnCards] };
-                }
-                return p;
-            }));
-            setCurrentPlayerId(getNextPlayerId());
-            setCardEffect(false);
-            setNextStockDrawCount(1);
-        }
+        const result = tryPlayCardArtificialPlayer();
+        if (result) { return; }
+        const [stockAfterDrawing, discardPileAfterDrawing, drawnCards] = drawCards(stock, discardPile, nextStockDrawCount);
+        setStock(stockAfterDrawing);
+        setDiscardPile(discardPileAfterDrawing);
+        setPlayers(players.map(p => {
+            if (p.id === currentPlayerId) {
+                return { ...p, cards: [...p.cards, ...drawnCards] };
+            }
+            return p;
+        }));
+        setCurrentPlayerId(getNextPlayerId(players, currentPlayerId));
+        setCardEffect(false);
+        setNextStockDrawCount(1);
     }
 
     function tryPlayCardArtificialPlayer() {
-        const foundCard = getCurrentPlayer().cards
+        const foundCard = getCurrentPlayer(players, currentPlayerId, beginRound).cards
             .find(card => canBePlayedOn(card, discardPile, activeSuit, isCardEffectOn));
         if (foundCard === undefined) {
             return false;
         }
-        const willPlayerBeRemoved = getCurrentPlayer().cards.length === 1;
-        if (players.length === 2 && willPlayerBeRemoved) {
-            //Game over: failure
-            setBeginRound(true);
-            return true;
-        }
         setDiscardPile([...discardPile, foundCard]);
-        const playersAfterPlayedCardRemoval = players.map(p => {
-            if (p.id === currentPlayerId) {
-                return { ...p, cards: p.cards.filter(c => !getImageSrc(foundCard).includes(getImageSrc(c))) };
-            }
-            return p;
-        });
-        const playersAfterPossibleCurrentPlayerRemoval = willPlayerBeRemoved
-            ? playersAfterPlayedCardRemoval.filter(p => currentPlayerId !== p.id)
-            : playersAfterPlayedCardRemoval;
-        setCurrentPlayerId(getNextPlayerId());
-        setPlayers(playersAfterPossibleCurrentPlayerRemoval);
+        setCurrentPlayerId(getNextPlayerId(players, currentPlayerId));
+        setPlayers(players
+            .map(p => {
+                if (p.id === currentPlayerId) {
+                    return { ...p, cards: p.cards.filter(c => !getImageSrc(foundCard).includes(getImageSrc(c))) };
+                }
+                return p;
+            })
+            .filter(p => p.cards.length !== 0));
         setCardEffect(foundCard.rank === "7" || foundCard.rank === "A");
-        const newSuit = foundCard.rank === "J"
+        const newSuit = foundCard.rank === "Q"
             ? SUITS[Math.floor(Math.random() * 100) % 4]
             : foundCard.suit;
         setActiveSuit(newSuit);
         setNextStockDrawCount(getNextStockDrawCount(foundCard.rank === "7" || foundCard.rank === "A", foundCard, nextStockDrawCount));
+        setRenderFinishModal(players.length === 2 && getCurrentPlayer(players, currentPlayerId, beginRound).cards.length === 1);
         return true;
     }
 
     const [topPlayerCards, leftPlayerCards, rightPlayerCards, bottomPlayerCards] =
-        (["top", "left", "right", "bottom"] as const).map(getPlayerCards);
+        (["top", "left", "right", "bottom"] as const).map(position => getPlayerCards(players, position));
 
-    function getPlayerCards(position: Position) {
-        return players.find(p => p.position === position)?.cards ?? [];
+    let modalContentClass = "modal__content modal__content--finish";
+    let closeBtnClass = "close-btn close-btn--finish";
+    let text = "You finished";
+    if (players.length === 3 && !players.find(p => p.isReal) && renderFinishModal) {
+        modalContentClass = "modal__content modal__content--win";
+        closeBtnClass = "close-btn close-btn--win";
+        text = "You won!";
+    }
+    if (players.length === 1 && players[0].isReal && renderFinishModal) {
+        modalContentClass = "modal__content modal__content--loss";
+        closeBtnClass = "close-btn close-btn--loss";
+        text = "It does not matter how slowly you go, as long as you don\’t stop";
     }
 
     return (
@@ -226,7 +236,7 @@ export function App(props: {}) {
                 <PlayState discardPileTopSrc={getImageSrc(discardPile.at(-1))}
                     onStockClick={handleCardClick}
                     activeSuit={activeSuit}
-                    activePlayer={getCurrentPlayer()}
+                    activePlayer={getCurrentPlayer(players, currentPlayerId, beginRound)}
                     nextDrawCount={nextStockDrawCount} />
                 <Player isReal={false} position="right" cards={rightPlayerCards} onCardClick={() => { }} />
             </PlayBoardArea>
@@ -234,6 +244,8 @@ export function App(props: {}) {
                 <Button text="Skip turn" onClick={handleSkipTurnClick} />
                 <Player isReal position="" cards={bottomPlayerCards} onCardClick={handleCardClick} />
             </PlayBoardArea>
+            {renderFinishModal && <Modal modalContentClass={modalContentClass} closeBtnClass={closeBtnClass} text={text} onClick={handleModalClose} />}
+            {renderSuitChoiceModal && <SuitChoiceModal onSuitImageClick={handleSuitChoice} />}
         </>
     );
 }
@@ -250,14 +262,15 @@ function drawCards(stock: CardType[], discardPile: CardType[], drawCount: number
     const newStock = discardPile.slice(0, -1).reverse();
     const newDiscardPile = discardPile.slice(-1);
     const drawnCards = [...stock.slice(), ...newStock.slice(-(drawCount - stock.length))];
-    return [newStock, newDiscardPile, drawnCards];
+    const stockAfterDrawing = newStock.slice(0, -(drawCount - stock.length))
+    return [stockAfterDrawing, newDiscardPile, drawnCards];
 }
 
 function canBePlayedOn(card: CardType, discardPile: CardType[], activeSuit: Suit, isCardEffectOn: boolean) {
     if (!isCardEffectOn) {
         return card.suit === activeSuit
             || card.rank === discardPile.at(-1)?.rank
-            || card.rank === "J";
+            || card.rank === "Q";
     }
     if (discardPile.at(-1)?.rank === "A") {
         return card.rank === "A";
@@ -279,4 +292,24 @@ function getNextStockDrawCount(isCardEffectOn: boolean, discardPileTop: CardType
         return nextStockDrawCount + (nextStockDrawCount % 2 === 0 ? 2 : 1);
     }
     return 1;
+}
+
+function getCurrentPlayer(players: PlayerType[], currentPlayerId: string, beginRound: boolean): PlayerType {
+    const player = players.find(p => p.id === currentPlayerId);
+    if (player === undefined) {
+        if (!beginRound) {
+            throw Error("Player with current player id isn't found");
+        }
+        return { id: "", isReal: false, cards: [], position: "left" };
+    }
+    return player;
+}
+
+function getNextPlayerId(players: PlayerType[], currentPlayerId: string) {
+    const index = players.findIndex(p => p.id === currentPlayerId);
+    return players[(index + 1) % players.length].id;
+}
+
+function getPlayerCards(players: PlayerType[], position: Position) {
+    return players.find(p => p.position === position)?.cards ?? [];
 }
---
+++ src/Modal.tsx
@@ -0,0 +1,30 @@
+import { Suit } from './types/card';
+import { getSuitImage } from './Utility';
+
+export function Modal(props: { modalContentClass: string, closeBtnClass: string, text: string, onClick: () => void }) {
+    return (
+        <div className="modal" onClick={props.onClick}>
+            <div className={props.modalContentClass} onClick={e => e.stopPropagation()}>
+                <span className={props.closeBtnClass} onClick={props.onClick}>&times;</span>
+                {props.text}
+            </div>
+        </div>
+    );
+}
+
+export function SuitChoiceModal(props: { onSuitImageClick: (target: HTMLImageElement) => void }) {
+    return (
+        <div className="modal">
+            <div className="modal__content">
+                <p>Choose the suit that must be played next:</p>
+                <div>
+                    {(["club", "heart", "spade", "diamond"] as Suit[]).map(suit =>
+                        <img className="modal__content__suit-img"
+                            src={getSuitImage(suit)}
+                            onClick={(e) => props.onSuitImageClick(e.target as HTMLImageElement)} />
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+}
